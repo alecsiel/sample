54f87c829cb409787a3d84d23d6ec54b0afccda7
wansoon <alecsiel@naver.com>
2013-04-23 16:43:41 +0900

    change memroy DB to file DB and add Comment Table

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -11,8 +11,20 @@ create table article (
   constraint pk_article primary key (id))
 ;
 
+create table comment (
+  id                        bigint not null,
+  name                      varchar(255),
+  text                      varchar(255),
+  article_id                bigint,
+  constraint pk_comment primary key (id))
+;
+
 create sequence article_seq;
 
+create sequence comment_seq;
+
+alter table comment add constraint fk_comment_article_1 foreign key (article_id) references article (id) on delete restrict on update restrict;
+create index ix_comment_article_1 on comment (article_id);
 
 
 
@@ -22,7 +34,11 @@ SET REFERENTIAL_INTEGRITY FALSE;
 
 drop table if exists article;
 
+drop table if exists comment;
+
 SET REFERENTIAL_INTEGRITY TRUE;
 
 drop sequence if exists article_seq;
+
+drop sequence if exists comment_seq;
 
